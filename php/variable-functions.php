b8a148af153af4c4b7d8c567a9a5f246e724034a
Josep Peñalba <jopema7@gmail.com>
2021-12-09 19:55:55 +0100

    Added variable functions

---
+++ php/variable-functions.php
@@ -0,0 +1,46 @@
+<?php
+
+
+
+class Foo
+{
+
+    public function __construct()
+    {
+        echo 'construct';
+    }
+
+    function foo(){
+        echo 'foo';
+    }
+
+    static function boo(){
+        echo 'boo';
+    }
+
+    public function __invoke()
+    {
+        echo 'invoked';
+    }
+}
+
+$c = 'Foo';
+$foo = new $c(); // construct
+$f = 'foo';
+$foo->$f(); // foo
+$foo(); // invoked
+
+class Manager{
+    public function executeCallable(callable $callable){
+        call_user_func($callable);
+    }
+}
+
+$m = new Manager();
+$m->executeCallable(function(){
+    echo "hi";
+}); // hi
+$m->executeCallable(array('Foo','boo')); // boo
+$m->executeCallable(new Foo()); // construct invoked
+$m->executeCallable(new $c()); // construct invoked
+$m->executeCallable(array(new $c(), 'foo')); // construct foo
