1a948ad6e11b38a2c10253b36c520b13b3f22757
Josep Peñalba <jopema7@gmail.com>
2024-09-13 17:56:15 +0200

    Next Marvel movie mini project

---
+++ php/next-film/templates/styles.php
@@ -0,0 +1,23 @@
+<style>
+    :root {
+        color-scheme: light dark;
+    }
+
+    body {
+        display: grid;
+        place-content: center;
+    }
+
+    section {
+        display: flex;
+        justify-content: center;
+        text-align: center;
+    }
+
+    hgroup {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+    }
+</style>
